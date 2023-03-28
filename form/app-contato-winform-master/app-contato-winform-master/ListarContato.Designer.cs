8536141c2a5fb9f450a3058542cf04e30a510921
Thauany Celestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-03-28 17:04:27 -0400

    form app contato

---
+++ form/app-contato-winform-master/app-contato-winform-master/ListarContato.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AppContatoForm
+{
+    partial class ListarContato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvContato = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContato)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvContato
+            // 
+            this.dgvContato.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContato.Location = new System.Drawing.Point(41, 24);
+            this.dgvContato.Name = "dgvContato";
+            this.dgvContato.Size = new System.Drawing.Size(814, 436);
+            this.dgvContato.TabIndex = 0;
+            // 
+            // ListarContato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 493);
+            this.Controls.Add(this.dgvContato);
+            this.Name = "ListarContato";
+            this.Text = "LISTAR CONTATO";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContato)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvContato;
+    }
+}
